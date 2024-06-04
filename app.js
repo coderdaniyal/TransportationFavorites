9205c2d992fb0b8c73aaca2e1cf378599ba06940
codewithdaniyal <167603648+codewithdaniyal@users.noreply.github.com>
2024-06-04 18:23:10 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+var transport = ['Honda125', 'Land Crusier', 'HondaCD70', 'Toyota'];
+for (var _i = 0, transport_1 = transport; _i < transport_1.length; _i++) {
+    var elements = transport_1[_i];
+    console.log("I would like to own on a ".concat(elements));
+}
